789b6c38aed2c290f2fb89226e64674303df596d
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-02-14 16:57:11 -0300

    Update aula21.py

---
+++ aula21.py
@@ -0,0 +1,27 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições precisam ser
+# verdadeiras.
+# Se qualquer valor for considerado falso,
+# a expressão inteira será avaliada naquele valor
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None (significa que não existe) que é
+# usado para representar um não valor
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+entry = input("Digite: 'E' para entrar | 'S' para sair ")
+confirm_password = "12345"
+
+if entry == 'E' or entry =='e': 
+    password = input("Senha: ")
+    if password == confirm_password:
+        print("Entrou")
+    else:
+        print("Senha incorreta")
+else:
+    print("Saiu")
+
+# Avaliação de curto circuito
+print(True and True and False)
