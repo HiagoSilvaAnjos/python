b561a80d5b71a8393a6fa6e34f985ba15bde08c1
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-03-04 22:37:09 -0300

    reviw

--- aula48.py
+++ aula48.py
@@ -3,21 +3,19 @@ Listas em Python
 Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
-Métodos úteis: append, insert, pop, del, clear, extend, +
-"""  
-#        +01234
-#        -54321  
-string = "ABCDE" # 5 caracteres (len)
-# print(boll([])) # falsy
-
-listItens = [123, True, "Hiagozera", 1.3, ["list dentro de outra list"]]
-print(listItens)
-
-print(30 * "=")
-
-for item in listItens:
-
-    if (type(item) == list):
-        print(item[0])
-    else:
-        print(item)
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
