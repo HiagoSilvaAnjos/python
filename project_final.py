d9ec88fc4cf52d36ea0ab1b8b224f5aa65c66f67
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-10-09 22:08:08 -0300

    remove colorama

--- project_final.py
+++ project_final.py
@@ -1,6 +1,4 @@
 import os
-from colorama import init, Fore, Style
-init()
 
 games = []
 kill_programing = False
@@ -16,21 +14,21 @@ menu_list = """
 
 def handle_add_game():
     os.system("clear")
-    name = input(Fore.YELLOW + "Nome do game: " + Style.RESET_ALL)
-    year = int(input(Fore.YELLOW + "Ano de lançamento: " + Style.RESET_ALL))
-    price = float(input(Fore.YELLOW + "Preço do game: " + Style.RESET_ALL))
+    name = input("Nome do game: ")
+    year = int(input("Ano de lançamento: "))
+    price = float(input("Preço do game: "))
     game = {"name": name, "year": year, "price": price}
     games.append(game)
     os.system("clear")
-    print(Fore.GREEN + "Game cadastrado com sucesso!" + Style.RESET_ALL)
+    print("Game cadastrado com sucesso!")
 
 def search_games():
     os.system("clear")
 
     if not games:
-        return print(Fore.RED + "Lista de games vazia..." + Style.RESET_ALL)    
+        return print("Lista de games vazia...")    
 
-    name = input(Fore.YELLOW + "Nome do game: " + Style.RESET_ALL)
+    name = input("Nome do game: ")
     len_list = len(games)
     found_game = False
     for game_index in range(len_list):
@@ -41,13 +39,13 @@ def search_games():
             print(f"Ano de lançamento: {get_game['year']}")
             print(f"Preço: R${get_game['price']}")
     if not found_game:
-        print(Fore.RED + "Game não encontrado." + Style.RESET_ALL )
+        print("Game não encontrado." )
 
 def games_list():
     os.system("clear")
 
     if not games:
-        return print(Fore.RED + "Lista de games vazia..." + Style.RESET_ALL)
+        return print("Lista de games vazia...")
 
     len_list = len(games)
     print("Todos os games cadastrados:")
@@ -55,21 +53,21 @@ def games_list():
         get_game = games[game_index]
         print(f"Nome: {get_game['name']}")
         print(f"Ano de lançamento: {get_game['year']}")
-        print(f"Preço: {get_game['price']}\n")
+        print(f"Preço: R${get_game['price']}\n")
         print()
 
 def find_interval_games():
     os.system("clear")
 
     if not games:
-        return print(Fore.RED + "Lista de games vazia..." + Style.RESET_ALL)
+        return print("Lista de games vazia...")
     
-    start_index = int(input(Fore.YELLOW + "Índice inicial: " + Style.RESET_ALL))
-    end_index = int(input(Fore.YELLOW + "Índice final: " + Style.RESET_ALL))
+    start_index = int(input("Índice inicial: "))
+    end_index = int(input("Índice final: "))
     len_list = len(games)
 
     if start_index >= 0 and end_index < len_list and start_index <= end_index:
-        print(Fore.GREEN + "Trecho da lista: " + Style.RESET_ALL)
+        print("Trecho da lista: ")
         for game_index in range(start_index, end_index + 1):
             get_game = games[game_index]
             print(f"Nome: {get_game['name']}")
@@ -77,69 +75,70 @@ def find_interval_games():
             print(f"Preço: R${get_game['price']}")
             print()
     else:
-        print(Fore.RED +  "Intervalo inválido." + Style.RESET_ALL)
+        print(f"Intervalo inválido.")
+        print(f"Tamanho da sua lista de games: {len_list}")
 
 def remove_game_to_list():
     os.system("clear")
     
     if not games:
-        return print(Fore.RED + "Lista de games vazia..." + Style.RESET_ALL)
+        return print("Lista de games vazia...")
 
-    game_name = input(Fore.YELLOW + "Digite o nome do game a remover: " + Style.RESET_ALL)
+    game_name = input("Digite o nome do game a remover: ")
     len_list = len(games)
     
     for game_index in range(len_list):
         current_game = games[game_index]
         if current_game["name"].lower() == game_name.lower():
             games.remove(current_game)
-            print(Fore.GREEN + f"Game removido com sucesso: {current_game}" + Style.RESET_ALL)
+            print(f"Game removido com sucesso: {current_game}")
             return
-    print(Fore.RED + "Game não encontrado." + Style.RESET_ALL)
+    print("Game não encontrado.")
 
 while not kill_programing:
-    print(Fore.CYAN + menu_list + Style.RESET_ALL)
-    option = input(Fore.YELLOW + "Escolha uma opção: " + Style.RESET_ALL)
+    print(menu_list)
+    option = input("Escolha uma opção: ")
 
     if option == "1":
         if len(games) >= 50:
-            print(Fore.RED + "Limite de jogos atingido! Não é possível cadastrar mais." + Style.RESET_ALL)
+            print("Limite de jogos atingido! Não é possível cadastrar mais.")
             continue
         try:
             handle_add_game()
         except Exception as e:
-            print(Fore.RED + "Você inseriu dados inválidos! Por favor, tente novamente..." + Style.RESET_ALL)
+            print("Você inseriu dados inválidos! Por favor, tente novamente...")
             continue
        
     if option == "2":
         try:
             search_games()
         except Exception as e:
-            print(Fore.RED + "Falha ao buscar game! Por favor, tente novamente..." + Style.RESET_ALL)
+            print("Falha ao buscar game! Por favor, tente novamente...")
             continue
 
     if option == "3":
         try:
             games_list()
         except Exception as e:
-            print(Fore.RED + "Falha ao carregar games! Por favor, tente novamente..." + Style.RESET_ALL)
+            print("Falha ao carregar games! Por favor, tente novamente...")
             continue
 
     if option == "4":
         try:
             find_interval_games()
         except Exception as e:
-            print(Fore.RED + "Você inseriu dados inválidos! Por favor, tente novamente..." + Style.RESET_ALL)
+            print("Você inseriu dados inválidos! Por favor, tente novamente...")
             continue        
 
     if option == "5":
         try:
             remove_game_to_list()
         except Exception as e:
-            print(Fore.RED + "Você inseriu dados inválidos! Por favor, tente novamente..." + Style.RESET_ALL)
+            print("Você inseriu dados inválidos! Por favor, tente novamente...")
             print(e)
             continue           
 
     if option == "6":
         os.system("clear")
         kill_programing = True
-        print(Fore.BLUE + "Saindo do sistema..." + Style.RESET_ALL)
+        print("Saindo do sistema...")
