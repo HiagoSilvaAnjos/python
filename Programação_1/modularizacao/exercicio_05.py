77c586965fee22ffbdcdddd3fe51ce2595cfbb67
Hialagol <hiagosilvaanjos7@gmail.com>
2024-12-16 08:50:53 -0300

    .

--- Programação_1/modularizacao/exercicio_05.py
+++ Programação_1/modularizacao/exercicio_05.py
@@ -1,25 +1,25 @@
-def buscar_elemento(vetor, n, a):
+def buscar_elemento(vetor, tamanhoLista, itemBuscado):
 
-    for i in range(n):
-        if vetor[i] == a:
-            return i  
+    for i in range(tamanhoLista):
+        if vetor[i] == itemBuscado:
+            return itemBuscado 
     return "Não encontrado" 
 
 def main():
     try:
-        n = int(input("Digite o tamanho do vetor: "))
+        tamanhoLista = int(input("Digite o tamanho do vetor: "))
 
-        print(f"Digite {n} números para o vetor separados por espaço:")
+        print(f"Digite {tamanhoLista} números para o vetor separados por espaço:")
         vetor_str = input().split()
         vetor = [int(num) for num in vetor_str]  
 
-        if len(vetor) != n:
+        if len(vetor) != tamanhoLista:
             print("Erro: O número de elementos não corresponde ao tamanho informado.")
             return
 
-        a = int(input("Digite o valor a ser buscado: "))
+        itemBuscado = int(input("Digite o valor a ser buscado: "))
 
-        posicao = buscar_elemento(vetor, n, a)
+        posicao = buscar_elemento(vetor, tamanhoLista, itemBuscado)
 
         print(posicao)
     except ValueError:
