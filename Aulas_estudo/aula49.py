c2c7f9be8136a102e144cb037afb58372c59c0db
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-01-02 22:00:17 -0300

    novas aulas

--- Aulas_estudo/aula49.py
+++ Aulas_estudo/aula49.py
@@ -1,2 +1,15 @@
-# Exercício - exiba os índices da lista
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
 
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
