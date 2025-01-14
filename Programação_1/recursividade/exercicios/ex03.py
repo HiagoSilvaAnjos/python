cf8c8095ef03f5ff9c578b07e14271414bf7c3a2
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-01-13 22:41:22 -0300

    .

--- Programação_1/recursividade/exercicios/ex03.py
+++ Programação_1/recursividade/exercicios/ex03.py
@@ -3,7 +3,7 @@
 def sum_vector(vetor):
     if not vetor:  # Caso base: vetor vazio
         return 0
-    return vetor[0] + sum_vector(vetor[1:])  # Chamada recursiva com fatiamento
+    return vetor[0] + sum_vector(vetor[1:])  # Chamada recursiva com fatiamento (1 + (2 + (3 + (4 + 5)))) == 15
 
 vetor = [1, 2, 3, 4, 5]
-print(sum_vector(vetor))  # Saída: 15
+print(sum_vector(vetor))  # Saída: 15   
