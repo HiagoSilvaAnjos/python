64aed3f0ec83ea3999b68daad05442364a27a40b
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-01-13 22:51:29 -0300

    .

--- Programação_1/recursividade/exercicios/ex05.py
+++ Programação_1/recursividade/exercicios/ex05.py
@@ -1,11 +1,12 @@
-#TODO: 5. Faça uma função recursiva para calcular a potência e retorna o valor de x elevado a n. 
+#TODO: 5. Faça uma função recursiva para calcular a potência e retorna o valor de number elevado a n. 
 
-def power(x, n):
-    if n == 0:  # Caso base: qualquer número elevado a 0 é 1
+def power(base, exponent):
+    if exponent == 0:  # Caso base: qualquer número elevado a 0 é 1
         return 1
-    return x * power(x, n - 1)  # Chamada recursiva: reduz n e multiplica x
+    return base * power(base, exponent - 1)  # Chamada recursiva (2 * (2 * (2 * (2 * (2 * 1))))) == 32
 
 # Exemplo de uso
-x = 2
-n = 5
-print(power(x, n))  # Saída: 32 (2^5 = 32)
+base = 2
+exponent = 5
+print(power(base, exponent))  # Saída: 32 (2^5 = 32)
+
