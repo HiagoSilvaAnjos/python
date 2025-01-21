f23eb2fd6fe1f0b48e71deb65480bfb3de67dd91
Hialagol <hiagosilvaanjos7@gmail.com>
2025-01-21 10:07:57 -0300

    .

--- Programação_1/Busca_em_vetores/busca_binaria_recursiva.py
+++ Programação_1/Busca_em_vetores/busca_binaria_recursiva.py
@@ -1,16 +1,16 @@
-def buscaRecursiva(v, item, inicio, fim):
+def buscaRecursiva(vetor_list, item_buscado, inicio, fim):
 	if inicio <= fim:
 		meio = (inicio + fim) // 2
-		if v[meio] == item:
-			return (True, item, meio)
-		elif item < v[meio]:
-			return buscaRecursiva(v, item, inicio, meio - 1)
+		if vetor_list[meio] == item_buscado:
+			return (True, item_buscado, meio)
+		elif item_buscado < vetor_list[meio]:
+			return buscaRecursiva(vetor_list, item_buscado, inicio, meio - 1)
 		else:
-			return buscaRecursiva(v, item, meio + 1, fim)
+			return buscaRecursiva(vetor_list, item_buscado, meio + 1, fim)
 	else:
 		return False
 
-v = [3, 15, 70, 80, 150, 461]
-print(buscaRecursiva(v, 70, 0, (len(v) - 1)))
+vetor_list = [3, 15, 70, 80, 150, 461]
+print(buscaRecursiva(vetor_list, 70, 0, (len(vetor_list) - 1)))
 
-v = [3, 5, 6, 8, 11, 12, 14, 15, 17, 18]
+# v = [3, 5, 6, 8, 11, 12, 14, 15, 17, 18]
