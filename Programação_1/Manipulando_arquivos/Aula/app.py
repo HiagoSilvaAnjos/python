5206a1996771a52a0770eb0d2efd2a1e600882de
Hialagol <hiagosilvaanjos7@gmail.com>
2025-01-28 10:38:08 -0300

    .

--- Programação_1/Manipulando_arquivos/Aula/app.py
+++ Programação_1/Manipulando_arquivos/Aula/app.py
@@ -13,24 +13,35 @@
 #     print(f"Error: {e}")
 
 try:
-    with open("tecnologias", 'r') as file:
+    with open("/home/savio/Documentos/curso-python/Programação_1/Manipulando_arquivos/Aula/tecnologias", 'r') as file:
+
+        file.seek(0)
+        tecnologias = file.readlines()
+        formater_tecnologias = [tecnologia.rstrip() for tecnologia in tecnologias]
+
+        print(f"Quantidade de linhas: {len(formater_tecnologias)}")
+
+        for tecnologia in formater_tecnologias:
+            print(f"Tecnologia: {tecnologia} | quantidade de caracteres: {len(tecnologia)}")
+        
+        print()
+
+        file.seek(0)
         content = file.readline()
 
         while content:
-            content = content.rstrip()
-            print(content)
-            content = file.readline()
+            content = content.rstrip() #remover \n
+            print(f"Tecnologia: {content} | quantidade de caracteres: {len(content)}")
+            content = file.readline() #ler a proxima linha
 except FileNotFoundError:
     print("Arquivo não encontrado")
 except Exception as e:
     print(f"Error: {e}")
 
 print()
-print()
-print()
 
 try:
-    with open("languages", 'w') as languages:
+    with open("/home/savio/Documentos/curso-python/Programação_1/Manipulando_arquivos/Aula/languages", 'w') as languages:
         language = input("Digite uma linguagem de programação: ")
 
         while language != "java":
@@ -40,3 +51,11 @@ except FileNotFoundError:
     print("Arquivo não encontrado")
 except Exception as e:
     print(f"Error: {e}")
+
+with open("/home/savio/Documentos/curso-python/Programação_1/Manipulando_arquivos/Aula/message", 'r', encoding='utf-8') as file:
+    content = file.readlines()
+    formater_text = [text.rstrip() for text in content]
+    print(formater_text)
+    for line in formater_text:
+        if "faturamento" in line:
+            print(line)
